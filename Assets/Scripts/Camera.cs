a3049af1566231ed4552826737721d2b117e2cb2
MJ Lindsay <glindsay156@gmail.com>
2021-09-20 14:11:36 -0300

    Camera Update

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Submarine submarineSript;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //transform.position.y = submarineSript.transform.position.y;
+    }
+}
---
+++ Assets/Scripts/Submarine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Submarine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+
+    }
+
+}
