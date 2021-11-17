01a844a47bd94362d38174433d87d8fe374aff55
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-17 11:03:48 -0400

    added rotation to enemies

--- Assets/Scripts/HeavyEnemy.cs
+++ Assets/Scripts/HeavyEnemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HeavyEnemy : Enemy
 {   
     // Start is called before the first frame update
-    protected static float viewDistance = 5f;
+    protected static float viewDistance = 10f;
     void Start()
     {
         minY = -115;
@@ -19,7 +19,7 @@ public class HeavyEnemy : Enemy
         transform.position = new Vector2(-(Screen.width / 100) - 2, randomY);
 
         health = 40;
-        attackDamage = 25;
+        attackDamage = 35;
         speed = Random.Range(1, 3);
         maxHealth = health;
         state = State.roam;
@@ -54,6 +54,13 @@ public class HeavyEnemy : Enemy
     }
     private void Chase()
     {
+        //Vector3 diff = (target.transform.position - this.transform.position);
+        //float angle = Mathf.Atan2(diff.y, diff.x);
+        //transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+
+        float angle = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, (speed * 10) * Time.deltaTime);
         if (target.transform.position.y > this.transform.position.y + 1)
         {
             this.transform.Translate(0, 0.001f * speed, 0);
@@ -75,7 +82,7 @@ public class HeavyEnemy : Enemy
 
     private void Retreat()
     {
-       
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, 0)), (speed * 10) * Time.deltaTime);
         if (randomY > this.transform.position.y + 1)
         {
             this.transform.Translate(0, 0.001f * speed, 0);
